417357a655f06283d59af09ae58e87f5f2df7a72
son.nguyentruong1 <sonqp1999a@gmail.com>
2022-12-26 18:51:40 +0700

    build app

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Homepage.css";
+
+const HomePage: React.FC = () => {
+  return (
+    <>
+      <div className="wrapper">
+        <div style={{right: "100%", transform: "translateX(68px)"}}>
+        <div className="test "></div>
+        <div className="test test2"></div>
+        <div className="test test3"></div>
+        <div className="test test4"></div>
+        <div className="test test5"></div>
+        <div className="test test6"></div>
+        <div className="test test7"></div>
+        <div className="test test8"></div>
+        <div className="test "></div>
+        <div className="test test9"></div>
+        <div className="test test10"></div>
+        <div className="test test11"></div>
+        <div className="test test12"></div>
+        <div className="test test13"></div>
+        <div className="test test14"></div>
+        <div className="test test15"></div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
