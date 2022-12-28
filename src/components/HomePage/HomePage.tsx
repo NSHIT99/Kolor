75b0d50304b4a1bac98828d4cfdea9f4e378772a
hoang.nguyencanh <hoang.nguyencanh@ncc.asia>
2022-12-28 14:54:56 +0700

    homepage v2

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -19,46 +19,66 @@ const HomePage: React.FC = () => {
               <div className="test test7"></div>
               <div className="test test8"></div>
             </div>
-            {/* <div className="testb test9"></div>
-            <div className="testb test10"></div>
-            <div className="testb test11"></div>
-            <div className="testb test12"></div>
-            <div className="testb test13"></div>
-            <div className="testb test14"></div>
-            <div className="testb test15"></div>
-            <div className="testb test16"></div> */}
-            {/* <div className="testc test17"></div>
-            <div className="testc test18"></div>
-            <div className="testc test19"></div>
-            <div className="testc test20"></div>
-            <div className="testc test21"></div>
-            <div className="testc test22"></div>
-            <div className="testc test23"></div>
-            <div className="testc test24"></div> */}
-            {/* <div className="test "></div>
-            <div className="test test2"></div>
-            <div className="test test3"></div>
-            <div className="test test4"></div>
-            <div className="test test5"></div>
-            <div className="test test6"></div>
-            <div className="test test7"></div>
-            <div className="test test8"></div>
-            <div className="testb "></div>
-            <div className="testb test2"></div>
-            <div className="testb test3"></div>
-            <div className="testb test4"></div>
-            <div className="testb test5"></div>
-            <div className="testb test6"></div>
-            <div className="testb test7"></div>
-            <div className="testb test8"></div>
-            <div className="testc "></div>
-            <div className="testc test2"></div>
-            <div className="testc test3"></div>
-            <div className="testc test4"></div>
-            <div className="testc test5"></div>
-            <div className="testc test6"></div>
-            <div className="testc test7"></div>
-            <div className="testc test8"></div> */}
+            <div className="chip-g2">
+              <div className="testb test9"></div>
+              <div className="testb test10"></div>
+              <div className="testb test11"></div>
+              <div className="testb test12"></div>
+              <div className="testb test13"></div>
+              <div className="testb test14"></div>
+              <div className="testb test15"></div>
+              <div className="testb test16"></div>
+            </div>
+            <div className="chip-g3">
+              <div className="testc test17"></div>
+              <div className="testc test18"></div>
+              <div className="testc test19"></div>
+              <div className="testc test20"></div>
+              <div className="testc test21"></div>
+              <div className="testc test22"></div>
+              <div className="testc test23"></div>
+              <div className="testc test24"></div>
+            </div>
+            <div className="chip-g4">
+              <div className="test test25"></div>
+              <div className="test test26"></div>
+              <div className="test test27"></div>
+              <div className="test test28"></div>
+              <div className="test test29"></div>
+              <div className="test test30"></div>
+              <div className="test test31"></div>
+              <div className="test test32"></div>
+            </div>
+            <div className="chip-g5">
+              <div className="testb test33"></div>
+              <div className="testb test34"></div>
+              <div className="testb test35"></div>
+              <div className="testb test36"></div>
+              <div className="testb test37"></div>
+              <div className="testb test38"></div>
+              <div className="testb test39"></div>
+              <div className="testb test40"></div>
+            </div>
+            <div className="chip-g6">
+              <div className="testc test41"></div>
+              <div className="testc test42"></div>
+              <div className="testc test43"></div>
+              <div className="testc test44"></div>
+              <div className="testc test45"></div>
+              <div className="testc test46"></div>
+              <div className="testc test47"></div>
+              <div className="testc test48"></div>
+            </div>
+            <div className="chip-g7">
+              <div className="test test49"></div>
+              <div className="test test50"></div>
+              <div className="test test51"></div>
+              <div className="test test52"></div>
+              <div className="test test53"></div>
+              <div className="test test54"></div>
+              <div className="test test55"></div>
+              <div className="test test56"></div>
+            </div>
           </div>
           <div className="bars">
             <div
